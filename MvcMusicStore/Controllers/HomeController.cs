99fb412fd44dba6bb62521cd28f75e7604a05647
aceymer <ljb@easv.dk>
2014-08-06 12:03:21 +0200

    Initial commit

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MvcMusicStore.Models;
+using MvcMusicStore.Models.FakeDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int id)
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Details = new FakeDB().GetDetail(id);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/Models/FakeDB/FakeDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models.FakeDB
+{
+    public class FakeDB
+    {
+        private Dictionary<int, Detail> Details = new Dictionary<int, Detail>();
+
+        public FakeDB() {
+            Details.Add(1, new Detail(1) { Name="TheName1" });
+            Details.Add(2, new Detail(2) { Name = "TheName2" });
+            Details.Add(3, new Detail(3) { Name = "TheName3" });
+            Details.Add(4, new Detail(4) { Name = "TheName4" });
+            Details.Add(5, new Detail(5) { Name = "TheName5" });
+            Details.Add(6, new Detail(6) { Name = "TheName6" });
+        }
+
+        public Detail GetDetail(int detailId) { 
+            return Details.ContainsKey(detailId) ? Details[detailId] : null;
+        }
+    }
+}
