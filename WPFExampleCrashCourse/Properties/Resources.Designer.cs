d4adf7c684c4f687353ae8bf16b87bf4ad7c44cd
aceymer <ljb@easv.dk>
2014-08-18 13:19:20 +0200

    Moved WPF

--- WPFExampleCrashCourse/Properties/Resources.Designer.cs
+++ WPFExampleCrashCourse/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MusicStoreWPF.Properties {
+namespace WPFExampleCrashCourse.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MusicStoreWPF.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MusicStoreWPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFExampleCrashCourse.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
